c72923f31cc4af8d8e65347634284d0888032821
MuriloCB <murilo.esker@gmail.com>
2024-06-20 13:24:12 -0300

    chore: update libs versions and docker.

--- ms-rails/run.sh
+++ ms-rails/run.sh
@@ -1,6 +1,7 @@
-#! /bin/ash
+#! /bin/bash
 export RAILS_MASTER_KEY=2c8e51116b8f319c236cbbe890720420
 export NEW_RELIC_AGENT_ENABLED=false 
 export RAILS_ENV=development 
 export RAILS_MAX_THREADS=5
+bundle exec rails db:migrate
 bundle exec puma -C config/puma.rb
